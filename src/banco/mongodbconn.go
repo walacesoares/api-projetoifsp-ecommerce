1f2ce407822b2632d172c439090c37a6c25ef7de
Walace Soares <walacesoares28@gmail.com>
2023-07-14 13:25:46 -0300

    Update mongodbconn.go

--- src/banco/mongodbconn.go
+++ src/banco/mongodbconn.go
@@ -15,8 +15,7 @@ var (
 
 func GetCollection(collection string) *mongo.Collection {
 
-	uri := fmt.Sprintf("mongodb+srv://admin:admin@cluster0.ctsu4.mongodb.net/?retryWrites=true&w=majority")
-	// uri := fmt.Sprintf("mongodb+srv://admin:admin@cluster0.ctsu4.mongodb.net/?retryWrites=true&w=majority")
+	uri := fmt.Sprintf("")
 
 	client, erro := mongo.NewClient(options.Client().ApplyURI(uri))
 	if erro != nil {
